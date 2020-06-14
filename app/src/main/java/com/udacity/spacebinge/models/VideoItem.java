ce9a1565a96362350accefec3db979fac2a85678
Abhijeet Singh <redefinedabhi2@gmail.com>
2020-06-15 04:17:05 +0530

    New parameters added to VideoItem Class for download feature

--- app/src/main/java/com/udacity/spacebinge/models/VideoItem.java
+++ app/src/main/java/com/udacity/spacebinge/models/VideoItem.java
@@ -12,6 +12,17 @@ import java.util.List;
 @Entity(tableName = "VideoItems")
 public class VideoItem implements Parcelable {
 
+    public static final Creator<VideoItem> CREATOR = new Creator<VideoItem>() {
+        @Override
+        public VideoItem createFromParcel(Parcel in) {
+            return new VideoItem(in);
+        }
+
+        @Override
+        public VideoItem[] newArray(int size) {
+            return new VideoItem[size];
+        }
+    };
     @PrimaryKey(autoGenerate = true)
     private int id;
     private String title;
@@ -22,6 +33,66 @@ public class VideoItem implements Parcelable {
     private String media_type;
     private String date_created;
     private List<String> keywords;
+    private boolean is_downloaded;
+    private String storage_path;
+
+    public VideoItem(int id,
+                     String title,
+                     String description,
+                     String video_url,
+                     String nasa_id,
+                     String href,
+                     String media_type,
+                     String date_created,
+                     List<String> keywords) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.video_url = video_url;
+        this.nasa_id = nasa_id;
+        this.href = href;
+        this.media_type = media_type;
+        this.date_created = date_created;
+        this.keywords = keywords;
+        this.is_downloaded = false;
+        this.storage_path = "";
+
+    }
+
+    @Ignore
+    public VideoItem(String title,
+                     String description,
+                     String video_url,
+                     String nasa_id,
+                     String href,
+                     String media_type,
+                     String date_created,
+                     List<String> keywords) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.video_url = video_url;
+        this.nasa_id = nasa_id;
+        this.href = href;
+        this.media_type = media_type;
+        this.date_created = date_created;
+        this.keywords = keywords;
+        this.is_downloaded = false;
+        this.storage_path = "";
+    }
+
+    protected VideoItem(Parcel in) {
+        id = in.readInt();
+        title = in.readString();
+        description = in.readString();
+        video_url = in.readString();
+        nasa_id = in.readString();
+        href = in.readString();
+        media_type = in.readString();
+        date_created = in.readString();
+        is_downloaded = in.readBoolean();
+        storage_path = in.readString();
+    }
 
     public int getId() {
         return id;
@@ -59,55 +130,20 @@ public class VideoItem implements Parcelable {
         return keywords;
     }
 
-    public VideoItem(int id,
-                     String title,
-                     String description,
-                     String video_url,
-                     String nasa_id,
-                     String href,
-                     String media_type,
-                     String date_created,
-                     List<String> keywords) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.video_url = video_url;
-        this.nasa_id = nasa_id;
-        this.href = href;
-        this.media_type = media_type;
-        this.date_created = date_created;
-        this.keywords = keywords;
+    public boolean isIs_downloaded() {
+        return is_downloaded;
     }
 
-    @Ignore
-    public VideoItem(String title,
-                     String description,
-                     String video_url,
-                     String nasa_id,
-                     String href,
-                     String media_type,
-                     String date_created,
-                     List<String> keywords) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.video_url = video_url;
-        this.nasa_id = nasa_id;
-        this.href = href;
-        this.media_type = media_type;
-        this.date_created = date_created;
-        this.keywords = keywords;
+    public void setIs_downloaded(boolean is_downloaded) {
+        this.is_downloaded = is_downloaded;
     }
 
-    protected VideoItem(Parcel in) {
-        id = in.readInt();
-        title = in.readString();
-        description = in.readString();
-        video_url = in.readString();
-        nasa_id = in.readString();
-        href = in.readString();
-        media_type = in.readString();
-        date_created = in.readString();
+    public String getStorage_path() {
+        return storage_path;
+    }
+
+    public void setStorage_path(String storage_path) {
+        this.storage_path = storage_path;
     }
 
     @Override
@@ -120,19 +156,9 @@ public class VideoItem implements Parcelable {
         dest.writeString(href);
         dest.writeString(media_type);
         dest.writeString(date_created);
+        dest.writeString(String.valueOf(is_downloaded));
+        dest.writeString(storage_path);
     }
-
-    public static final Creator<VideoItem> CREATOR = new Creator<VideoItem>() {
-        @Override
-        public VideoItem createFromParcel(Parcel in) {
-            return new VideoItem(in);
-        }
-
-        @Override
-        public VideoItem[] newArray(int size) {
-            return new VideoItem[size];
-        }
-    };
 
     @Override
     public int describeContents() {

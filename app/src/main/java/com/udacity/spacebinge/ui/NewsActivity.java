dc42fb47856802a3c14fd0a5e630f4091af89477
Abhijeet Singh <redefinedabhi2@gmail.com>
2020-06-16 00:04:32 +0530

    Current Status: All planned activities skeleton created along with layouts

---
+++ app/src/main/java/com/udacity/spacebinge/ui/NewsActivity.java
@@ -0,0 +1,16 @@
+package com.udacity.spacebinge.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.udacity.spacebinge.R;
+
+public class NewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news);
+    }
+}

99bc81a895c9f839daaa6eb484bc086a454cea47
Abhijeet Singh <redefinedabhi2@gmail.com>
2020-06-15 23:59:44 +0530

    Current Status: Adding more activities

---
+++ app/src/main/java/com/udacity/spacebinge/ui/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.udacity.spacebinge.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.udacity.spacebinge.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}

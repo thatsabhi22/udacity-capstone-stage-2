39f5475a4b8222775951c3f8a8248f01961b7d95
Abhijeet Singh <redefinedabhi2@gmail.com>
2020-06-18 06:18:27 +0530

    Single Use case of retrieving latest data implemented

--- app/src/main/java/com/udacity/spacebinge/ui/MainActivity.java
+++ app/src/main/java/com/udacity/spacebinge/ui/MainActivity.java
@@ -3,16 +3,26 @@ package com.udacity.spacebinge.ui;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 import com.udacity.spacebinge.R;
+import com.udacity.spacebinge.models.VideoItem;
 import com.udacity.spacebinge.repositories.VideoItemRepository;
 import com.udacity.spacebinge.tasks.SpaceWebService;
+import com.udacity.spacebinge.viewmodels.HomePageViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
     VideoItemRepository videoItemRepository;
+    HomePageViewModel homePageViewModel;
+    Observer<List<VideoItem>> videoItemObserver;
     private SpaceWebService spaceWebService;
+    private List<VideoItem> videoItemList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +33,21 @@ public class MainActivity extends AppCompatActivity {
 
         videoItemRepository = VideoItemRepository.getInstance(this);
         videoItemRepository.getVideoCollection("latest", "video");
+
+        initHomepageViewModel();
+        videoItemList = new ArrayList<>();
+
+    }
+
+    private void initHomepageViewModel() {
+        videoItemObserver =
+                movies -> {
+                    videoItemList.clear();
+                    videoItemList.addAll(movies);
+                };
+        homePageViewModel = ViewModelProviders.of(this)
+                .get(HomePageViewModel.class);
+        homePageViewModel.getLatest("latest", "video")
+                .observe(MainActivity.this, videoItemObserver);
     }
 }
--- app/src/main/java/com/udacity/spacebinge/viewmodels/HomePageViewModel.java
+++ app/src/main/java/com/udacity/spacebinge/viewmodels/HomePageViewModel.java
@@ -4,9 +4,23 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.udacity.spacebinge.models.VideoItem;
+import com.udacity.spacebinge.repositories.VideoItemRepository;
+
+import java.util.List;
 
 public class HomePageViewModel extends AndroidViewModel {
+
+    private VideoItemRepository videoItemRepository;
+
     public HomePageViewModel(@NonNull Application application) {
         super(application);
+        videoItemRepository = VideoItemRepository.getInstance(application.getApplicationContext());
+    }
+
+    public LiveData<List<VideoItem>> getLatest(String query, String media_type) {
+        return videoItemRepository.getVideoCollection(query, media_type);
     }
 }

1a2952270820ff1de9df844469b79dfcaf41436c
Abhijeet Singh <redefinedabhi2@gmail.com>
2020-07-08 18:10:53 +0530

    Offline mode handled on HomePage

--- app/src/main/java/com/udacity/spacebinge/ui/HomeActivity.java
+++ app/src/main/java/com/udacity/spacebinge/ui/HomeActivity.java
@@ -6,6 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,11 +22,13 @@ import com.udacity.spacebinge.adapters.HomePageAdapter;
 import com.udacity.spacebinge.models.VideoItem;
 import com.udacity.spacebinge.repositories.VideoItemRepository;
 import com.udacity.spacebinge.tasks.SpaceWebService;
+import com.udacity.spacebinge.utils.AppUtil;
 import com.udacity.spacebinge.viewmodels.HomePageViewModel;
 
 import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -35,10 +38,13 @@ public class HomeActivity extends AppCompatActivity {
     RecyclerView topicRV;
     HomePageAdapter homePageAdapter;
     Observer<LinkedHashMap<String, List<VideoItem>>> videoItemObserver;
+    ImageView loading_indicator_iv;
+    TextView offline_mode_tv, go_to_downloads_tv;
+    ImageView offline_mode_iv;
+    boolean isOffline;
     private SpaceWebService spaceWebService;
     private LinkedHashMap<String, List<VideoItem>> videoCollection;
     private List<String> topics;
-    ImageView loading_indicator_iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,25 +53,49 @@ public class HomeActivity extends AppCompatActivity {
 
         // Initializing all view elements in this method call
         initViewElements();
+        try {
+            isOffline = new AppUtil.CheckOnlineStatus().execute().get();
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
 
-        videoCollection = new LinkedHashMap<String, List<VideoItem>>();
-        topics = Arrays.asList(getResources().getStringArray(R.array.topics));
-        initHomepageViewModel();
+        if (isOffline) {
+            offline_mode_iv.setVisibility(View.VISIBLE);
+            offline_mode_tv.setVisibility(View.VISIBLE);
+            go_to_downloads_tv.setVisibility(View.VISIBLE);
 
-        homePageAdapter = new HomePageAdapter(this, videoCollection);
-        topicRV.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
-        topicRV.setAdapter(homePageAdapter);
+            go_to_downloads_tv.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(HomeActivity.this, DownloadActivity.class);
+                    startActivity(intent);
+                }
+            });
+        } else {
+            Glide
+                    .with(this)
+                    .asGif().load(R.drawable.globe_loading)
+                    .into(loading_indicator_iv);
 
-        // Register the AdapterdataObserver to the RecyclerView.Adapter
-        // onChanged would be called when data set of adapter changes
-        RecyclerView.AdapterDataObserver observer= new RecyclerView.AdapterDataObserver() {
-            @Override
-            public void onChanged() {
-                //Toast.makeText(HomeActivity.this,"recyclerview loaded",Toast.LENGTH_SHORT).show();
-                loading_indicator_iv.setVisibility(View.GONE);
-            }
-        };
-        homePageAdapter.registerAdapterDataObserver(observer);
+            videoCollection = new LinkedHashMap<String, List<VideoItem>>();
+            topics = Arrays.asList(getResources().getStringArray(R.array.topics));
+            initHomepageViewModel();
+
+            homePageAdapter = new HomePageAdapter(this, videoCollection);
+            topicRV.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
+            topicRV.setAdapter(homePageAdapter);
+
+            // Register the AdapterdataObserver to the RecyclerView.Adapter
+            // onChanged would be called when data set of adapter changes
+            RecyclerView.AdapterDataObserver observer = new RecyclerView.AdapterDataObserver() {
+                @Override
+                public void onChanged() {
+                    //Toast.makeText(HomeActivity.this,"recyclerview loaded",Toast.LENGTH_SHORT).show();
+                    loading_indicator_iv.setVisibility(View.GONE);
+                }
+            };
+            homePageAdapter.registerAdapterDataObserver(observer);
+        }
 
         BottomNavigationView bottomNavigationView
                 = (BottomNavigationView) findViewById(R.id.bottom_bar_navigation);
@@ -107,11 +137,10 @@ public class HomeActivity extends AppCompatActivity {
 
     private void initViewElements() {
         topicRV = findViewById(R.id.topics_recycler_view);
+        offline_mode_tv = findViewById(R.id.offline_mode_tv);
+        offline_mode_iv = findViewById(R.id.offline_mode_iv);
+        go_to_downloads_tv = findViewById(R.id.go_to_downloads_tv);
         loading_indicator_iv = findViewById(R.id.loading_indicator_home_iv);
-        Glide
-                .with(this)
-                .asGif().load(R.drawable.globe_loading)
-                .into(loading_indicator_iv);
     }
 
     private void initHomepageViewModel() {

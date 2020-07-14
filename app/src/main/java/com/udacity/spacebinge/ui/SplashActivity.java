3f70a5f547e1adc430b790b0f3c57f143ec770c7
Abhijeet Singh <redefinedabhi2@gmail.com>
2020-07-15 02:53:22 +0530

    Current Status: Splash Screen Loading indicator added

--- app/src/main/java/com/udacity/spacebinge/ui/SplashActivity.java
+++ app/src/main/java/com/udacity/spacebinge/ui/SplashActivity.java
@@ -1,16 +1,49 @@
 package com.udacity.spacebinge.ui;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
+import com.bumptech.glide.Glide;
 import com.udacity.spacebinge.R;
 
 public class SplashActivity extends AppCompatActivity {
+
+    ImageView splash_loading_indicator;
+    int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        splash_loading_indicator = findViewById(R.id.splash_loading_indicator);
+
+        Glide
+                .with(this)
+                .asGif().load(R.drawable.splash_progress)
+                .into(splash_loading_indicator);
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashActivity.this, SignUpActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
